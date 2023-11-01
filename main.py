03fa77f08db10d3001857ecc547045f7bf30a6c8
Omer Hyjaneh <hyajneh.1@osu.edu>
2023-11-01 17:13:22 -0400

    final fixes

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import sys
 from CoreScanner import CoreScanner
 
 def main(fileName):
+
+    # initiate object
     scanner = CoreScanner(fileName)
 
+    # get first token
     output_Number : int = None
 
+    # loop through tokens
     while (output_Number != 33) and (output_Number != 34):
         output_Number = scanner.getToken()
         print(output_Number)
         scanner.skipToken()
     
-
-
-
 
 if __name__ == "__main__":
     main(sys.argv[1])
